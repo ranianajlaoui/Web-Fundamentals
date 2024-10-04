319bd01e3c7fd6da500c6da6e31d8ca3c8321be0
unknown <ranianajlaoui69@gmail.com>
2024-10-04 15:24:46 -0700

    fizzbuzz

---
+++ Week_1/Day_5/Practice/fizbuzzz.js
@@ -0,0 +1,15 @@
+for( var i=0; i<100;i++){
+    if (i % 3 =0){
+        console.log(FiZZ)
+    }
+    if ( i % 5=0){
+        console.log(BUZZ)
+
+    }
+    if( i % 5 =0 && i % 3 = 0){
+        console.log(FIZZBUZZ)
+
+    }else {
+        console.log(i)
+    }
+}

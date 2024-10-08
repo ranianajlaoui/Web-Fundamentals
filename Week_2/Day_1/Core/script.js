36c02e3fe895c9508d04ad14f884ff58e07c63f8
unknown <ranianajlaoui69@gmail.com>
2024-10-08 11:33:08 -0700

    button clicker

---
+++ Week_2/Day_1/Core/script.js
@@ -0,0 +1,10 @@
+function clicked(element){
+    element.innerText=("logout");
+}
+function hide(element){
+    element.remove();
+
+}
+function alert(ninja was liked){
+    console.log("ninja was liked");
+}

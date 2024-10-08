ee3b984bc2064f2ffd0f648eccd4af1b62da00bb
unknown <ranianajlaoui69@gmail.com>
2024-10-08 13:43:43 -0700

    dojopizzaria

---
+++ Week_2/Day_1/Practice/dojo_pizzaria/script.js
@@ -0,0 +1,19 @@
+function pizzaOven(crustType, sauceType, cheeses,  toppings){
+    pizza.crustType=crustType;
+    pizza.saucetype=sauceType;
+    pizza.cheeses=cheeses;
+    pizza.toppings=toppings;
+
+} console.log(pizza);
+*****************************************************************************************
+var pizza= pizzaOven("deep dish", "traditional", ["mozzarella"], and ["pepperoni", "sausage"]){
+    console.log(pizza);
+}******************************************************************************************
+var pizza=pizzaOven("hand tossed", "marinara", ["mozzarella", "feta"], ["mushrooms", "olives", "onions"]){
+    console.log(pizza)
+}
+*****************************************************************************************
+var pizza=pizzaOven("Thick Crust","spanish sauce",["cheddar"],["fries"]){
+    console.log(pizza);
+}
+****************************************************************************************
